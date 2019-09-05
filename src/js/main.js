b01d856774408bd6ed5ca61a941ac6b713e8c00d
K.Tytsyk <noklen61@gmail.com>
2019-09-05 17:10:31 +0200

    header media

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,58 @@
 $(document).ready(function() {
-    function winSize () {
-        if ($(window).width() <= 768) {
-            $('.mobil-header').removeClass('header-none');
-            $('.top-menu').addClass('header-none');
-            $('.top-menu-login').addClass('header-none');
-        } else {
-            $('.mobil-header').addClass('header-none');
-            $('.top-menu').removeClass('header-none');
-            $('.top-menu-login').removeClass('header-none');
-        }
-    }
+    // function winSize () {
+    //     if ($(window).width() <= 768) {
+    //         $('.mobil-header').removeClass('header-none');
+    //         $('.top-menu').addClass('header-none');
+    //         $('.top-menu-login').addClass('header-none');
+    //     } else {
+    //         $('.mobil-header').addClass('header-none');
+    //         $('.top-menu').removeClass('header-none');
+    //         $('.top-menu-login').removeClass('header-none');
+    //     }
+    // }
+    //
+    // function moveGame () {
+    //     if ($(window).width() <= 1024) {
+    //         $("#games").appendTo("#gamesMobil");
+    //     } else {
+    //         $("#games").appendTo("#gameList");
+    //     }
+    // }
+    //
+    // $(".card").mouseenter(function() {
+    //     $('.games-list  #' + this.id + ' .overlay').addClass('overlay-flex');
+    // });
+    // $(".card").mouseleave(function() {
+    //     $('.games-list  #' + this.id + ' .overlay').removeClass('overlay-flex');
+    // });
+    //
+    // $(window).on('load resize',winSize);
+    // $(window).on('load resize',moveGame);
 
-    function moveGame () {
-        if ($(window).width() <= 1024) {
-            $("#games").appendTo("#gamesMobil");
-        } else {
-            $("#games").appendTo("#gameList");
-        }
-    }
+//     $('.winerCarousel').slick({
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 2000,
+// });
+//     $('.winer').slick ({
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 2000,
+//     });
 
-    $(".card").mouseenter(function() {
-        $('.games-list  #' + this.id + ' .overlay').addClass('overlay-flex');
-    });
-    $(".card").mouseleave(function() {
-        $('.games-list  #' + this.id + ' .overlay').removeClass('overlay-flex');
-    });
+        $('.icon-menu').click(function() {
+            $('.menu').animate({
+                left: '0px'
+            }, 200);
+        });
 
-    $(window).on('load resize',winSize);
-    $(window).on('load resize',moveGame);
-
-    $('.navicon').click(function(e){
-        $(this).next().slideToggle(500);
-    });
+        $('.icon-close').click(function() {
+            $('.menu').animate({
+                left: '-285px'
+            }, 200);
+        });
 
 
 });
